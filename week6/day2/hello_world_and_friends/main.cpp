e41ed5e28fd6d775eb850bbcd412454271a62b24
Jhona3 <jonas.kiss.3uk@gmail.com>
2019-05-14 11:58:04 +0200

    Hello me and Hello classmates task is done

---
+++ week6/day2/hello_world_and_friends/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, me!\n");
+
+    printf("Hello, Joco!\n");
+    printf("Hello, Alex!\n");
+    printf("Hello, Kenny!\n");
+
+    
+    return 0;
+}
